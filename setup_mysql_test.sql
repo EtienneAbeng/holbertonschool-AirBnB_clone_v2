bbd9bd1595a8943cc01a48d0bb369b12b6d7ed35
root <root@FAT>
2024-03-27 10:58:21 +0100

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,12 +2,10 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates user if doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
--- Sets password for user
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIZD BY 'hbnb_dev_pwd';
 -- Grants privileges to user on database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grants select privileges to user on performance_schema database
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- Flush privileges
 FLUSH PRIVILEGES;
