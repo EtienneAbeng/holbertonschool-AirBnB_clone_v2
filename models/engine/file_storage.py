00a7b45e3004e5f11803541527433806ea6555a8
EtienneAbeng <abengetienne@gmail.com>
2024-03-27 18:50:31 +0100

    add import filesStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
-
+import FileStorage
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
