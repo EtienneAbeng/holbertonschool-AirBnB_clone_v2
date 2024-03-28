a264e24032a4d0b09ba83dbdbf2b51cf7a60e249
root <root@FAT>
2024-03-28 13:48:22 +0100

    mon projet

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,11 @@ class BaseModel:
         """Instantiates a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
         if kwargs:
             for k, v in kwargs.items():
                 if k in ['created_at', 'updated_at']:
                     t_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    self.__dict__[k] = datetime.strptime(v, t_format)
+                    setattr(self, k, datetime.strptime(v, t_format))
                 elif k != '__class__':
                     setattr(self, k, v)
 
@@ -36,13 +35,12 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
--- models/state.py
+++ models/state.py
@@ -19,15 +19,8 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """return the cities of the current state"""
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+        cities_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return (cities_list)
