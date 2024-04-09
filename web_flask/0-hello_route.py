bf80546c68b3b81bb2f6a2fe1fe99e5e97d9811d
EtienneAbeng <abengetienne@gmail.com>
2024-04-09 14:16:23 +0200

    add a script that starts a Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+from flask import Flask  # Importation de la classe Flask depuis le module flask
+
+app = Flask(__name__)  # Création d'une instance de l'application Flask
+
+@app.route('/', strict_slashes=False)  # Définition d'une route pour l'URL '/'
+def hello_hbnb():  # Fonction exécutée lorsque l'URL '/' est accédée
+    return "Hello HBNB!"  # Retourne "Hello HBNB!" comme réponse à la requête
+
+if __name__ == '__main__':  # Vérification si le script est exécuté directement
+    app.run(host='0.0.0.0', port=5000)  # Démarrage du serveur Flask sur toutes les adresses IP (0.0.0.0) et le port 5000
