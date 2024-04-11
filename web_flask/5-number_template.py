ea971a5660a68fa38796a54b508d7cebc3da585a
EtienneAbeng <abengetienne@gmail.com>
2024-04-11 07:58:36 +0200

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,32 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  # Import des classes Flask et render_template
 
-app = Flask(__name__)
+app = Flask(__name__)  # Création de l'application Flask
 
-@app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)  # Route pour l'URL '/'
 def hello_hbnb():
-    return "Hello HBNB"
+    return "Hello HBNB"  # Retourne "Hello HBNB"
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)  # Route pour l'URL '/hbnb'
 def hbnb():
-    return "HBNB"
+    return "HBNB"  # Retourne "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)  # Route dynamique pour '/c/<text>'
 def c_is_fun(text):
-    return "C {}".format(text.replace('_', ''))
+    return "C {}".format(text.replace('_', ''))  # Retourne "C <text>" sans underscores
 
-@app.route('/python/', defaults ={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)  # Route dynamique pour '/python/<text>'
 def python_is_cool(text):
-    return "Python {}".format(text.repalce('_', ' '))
+    return "Python {}".format(text.replace('_', ' '))  # Retourne "Python <text>" sans underscores
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)  # Route dynamique pour '/number/<n>'
 def number(n):
-    return "{} is a number".format(n)
+    return "{} is a number".format(n)  # Retourne "<n> is a number"
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)  # Route dynamique pour '/number_template/<n>'
 def number_template(n):
-    return render_template('5-number.html', n = n)
-
-
+    return render_template('5-number.html', n=n)  # Rend le modèle '5-number.html' avec n comme variable
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)  # Démarrage du serveur Flask sur toutes les adresses IP et le port 5000
