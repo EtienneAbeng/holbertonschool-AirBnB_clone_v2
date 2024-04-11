b589b723b2398cdf4c0eda9cc643910b831b11df
EtienneAbeng <abengetienne@gmail.com>
2024-04-11 15:58:15 +0200

    add in app.run debug=TRUE

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,4 +49,4 @@ def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Démarrage du serveur Flask sur toutes les adresses IP et le port 5000
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Démarrage du serveur Flask sur toutes les adresses IP et le port 5000
