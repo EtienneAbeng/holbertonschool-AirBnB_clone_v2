08c902474ebf53d63dd694572b885a682f8678fa
EtienneAbeng <abengetienne@gmail.com>
2024-04-12 16:29:33 +0200

    Update indentation in all files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
-import json
-
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -11,48 +8,29 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            cls_dict = {}
-            for key, value in self.__objects.items():
-                if isinstance(value, cls):
-                    cls_dict[key] = value
-            return cls_dict
-        else:
-            return FileStorage.__objects
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        setattr(self.__objects, key, obj)
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+            temp = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    cls_name = val['__class__']
+                    cls = eval(cls_name)
+                    self.__objects[key] = cls(**val)
         except FileNotFoundError:
             pass
 
@@ -60,10 +38,8 @@ class FileStorage:
         """delete obj from __objects"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
+            self.__objects.pop(key, None)
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
-import models
-import shlex
 
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+
+
+Base = declarative_base()
 
 class State(BaseModel, Base):
-    """table of the class State"""
-    __tablename__ = "states"
+    """State class"""
+
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-    if getenv("HBNB_TYPE_STORAGE") != "db":
 
     @property
     def cities(self):
-        """return the cities of the current state"""
-        cities_list = []
-        for city in models.storage.all(City).values():
+        """Return the list of City instances related to the current state"""
+        from models import storage
+        from models.city import City
+
+        city_list = []
+        for city in storage.all(City).values():
             if city.state_id == self.id:
-                cities_list.append(city)
-        return (cities_list)
+                city_list.append(city)
+        return city_list
