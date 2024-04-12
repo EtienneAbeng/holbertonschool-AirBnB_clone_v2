1970a56ec1caa42fbf8d9e0491bc360487ac3c04
EtienneAbeng <abengetienne@gmail.com>
2024-04-12 13:05:44 +0200

    update indentation

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,17 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
-import models
-from sqlalchemy import column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+class State(BaseModel, Base):
+    """State class"""
 
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
-Base = declarative_base()
+    @property
+    def cities(self):
+        """Return the list of City instances related to the current state"""
+        from models import storage
+        from models.city import City
 
-
-class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, unique=True)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        if kwargs:
-            for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    t_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    setattr(self, k, datetime.strptime(v, t_format))
-                elif k != '__class__':
-                    setattr(self, k, v)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = self.__class__.__name__
-        return f'[{cls}] ({self.id}) {self.__dict__}'
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
-        return dictionary
-
-    def delete(self):
-        """Dete the current intance from the storage"""
-        models.storage.delete(self)
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
